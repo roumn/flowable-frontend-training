5f044994c809374770c6e649153e08bc688557de
matthias.stoeckli <matthias.stoeckli@flowable.com>
2024-07-26 14:45:29 +0200

    Update to 3.16 and simplify setup

--- frontend/src/form-components/suite.tsx
+++ frontend/src/form-components/suite.tsx
@@ -2,6 +2,7 @@ import {HelloWorld} from "./HelloWorld/HelloWorld";
 import {FlowableMap} from "./FlowableMap/FlowableMap";
 
 
+
 export default {
     trainingHelloworld: HelloWorld,
     trainingFlowableMap: FlowableMap
