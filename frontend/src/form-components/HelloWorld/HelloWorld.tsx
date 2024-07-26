e2d6882100da7d2d23905a3a67f75c2a65f7d901
matthias.stoeckli <matthias.stoeckli@flowable.com>
2024-07-26 14:45:29 +0200

    Update to 3.16 and simplify setup

--- frontend/src/form-components/HelloWorld/HelloWorld.tsx
+++ frontend/src/form-components/HelloWorld/HelloWorld.tsx
@@ -1,25 +1,18 @@
 import {_, Model} from '@flowable/forms';
-import React, {CSSProperties} from 'react';
+import React from 'react';
 
 export function HelloWorld(props: Model.Props) {
-    const Components = props.Components;
-    const {config} = props;
-    const {extraSettings, enabled} = config;
+    const {extraSettings} = props.config;
 
     // Used to generate style class names. See: https://getbem.com/introduction/
     const bem = _.bem("helloWorld");
 
     const locationToGreet = _.get(extraSettings, "locationToGreet", "World");
-    const greeting = _.get(extraSettings, "greeting", "Hello");
-    const style: CSSProperties = {
-        color: enabled == false ? "lightgrey" : "black"
-    }
 
     return (
         <div className={bem('container')}>
-            <Components.label {...props}/>
-            <div style={style}>
-                {greeting} <b>{locationToGreet}</b>
+            <div>
+                Hello <b>{locationToGreet}</b>
             </div>
         </div>
     );
