d7812f7c5df07c7eff46a32215543de1db4ed6e8
matthias.stoeckli <matthias.stoeckli@flowable.com>
2024-07-28 19:47:33 +0200

    Update stories

--- frontend/src/form-components/FlowableMap/FlowableMap.stories.tsx
+++ frontend/src/form-components/FlowableMap/FlowableMap.stories.tsx
@@ -1,19 +1,27 @@
 import * as React from "react";
 import {StorybookForm} from "../../util/storybookForm";
 
-export const ReadOnly = () =>
+export const Default = () =>
     <StorybookForm
-        payload={{location: [47.37, 8.54]}}
+        payload={{}}
         sfs={`
-            trainingFlowableMap: enabled=false value={{location}} size=12 [[center={{location}}]]        
+            trainingFlowableMap: enabled=false size=12 [[]]        
         `}
     />
 
-export const Editable = () =>
+export const Center = () =>
     <StorybookForm
-        payload={{location: [47.37, 8.54]}}
+        payload={{centerLon: 39.4962322, centerLat: -0.4011369}}
         sfs={`
-            trainingFlowableMap: enabled=true value={{location}} size=12 [[center={{location}}]]        
+            trainingFlowableMap: enabled=false size=12 [[centerLon={{centerLon}} centerLat={{centerLat}}]]        
+        `}
+    />
+
+export const Value = () =>
+    <StorybookForm
+        payload={{centerLon: 47.37, centerLat: 8.54}}
+        sfs={`
+            trainingFlowableMap: enabled=false value={{[centerLon, centerLat]}} size=12 [[centerLon={{location}} centerLat={{location}}]]        
         `}
     />
 
