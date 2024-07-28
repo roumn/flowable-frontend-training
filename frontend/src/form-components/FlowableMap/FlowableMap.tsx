2592bf188f101515f13c7ee3b8c51c8d8e2b2b9c
matthias.stoeckli <matthias.stoeckli@flowable.com>
2024-07-28 19:37:57 +0200

    Update map exercise

--- frontend/src/form-components/FlowableMap/FlowableMap.tsx
+++ frontend/src/form-components/FlowableMap/FlowableMap.tsx
@@ -1,6 +1,6 @@
 import {_, Model} from '@flowable/forms';
 import React from 'react';
-import {Map, Marker} from "pigeon-maps";
+import {Map, Marker, Point} from "pigeon-maps";
 
 export function FlowableMap(props: Model.Props) {
     const Components = props.Components;
@@ -9,7 +9,9 @@ export function FlowableMap(props: Model.Props) {
 
     // Used to generate style class names. See: https://getbem.com/introduction/
     const bem = _.bem("flowableMap");
-    const center = _.get(extraSettings, "center", []);
+    const centerLon: number = _.get(extraSettings, "centerLon", 47.37);
+    const centerLat: number = _.get(extraSettings, "centerLat", 8.54);
+    const center: Point = [centerLon, centerLat];
 
     const handleClick = (onClickEvent: { event: any, latLng: number[], pixel: any }) => {
         if(enabled) {
@@ -18,7 +20,7 @@ export function FlowableMap(props: Model.Props) {
     }
 
     if(config.__designMode == "editor") {
-        return <div style={{width:400, height:400, background: "gray", color: "white", textAlign: "center"}}>
+        return <div style={{width: 400, height: 400, background: "gray", color: "white", textAlign: "center"}}>
             Flowable Map</div>;
     }
 
--- frontend/src/form-components/HelloWorld/HelloWorld.tsx
+++ frontend/src/form-components/HelloWorld/HelloWorld.tsx
@@ -1,18 +1,25 @@
 import {_, Model} from '@flowable/forms';
-import React from 'react';
+import React, {CSSProperties} from 'react';
 
 export function HelloWorld(props: Model.Props) {
-    const {extraSettings} = props.config;
+    const Components = props.Components;
+    const {config} = props;
+    const {extraSettings, enabled} = config;
 
     // Used to generate style class names. See: https://getbem.com/introduction/
     const bem = _.bem("helloWorld");
 
     const locationToGreet = _.get(extraSettings, "locationToGreet", "World");
+    const greeting = _.get(extraSettings, "greeting", "Hello");
+    const style: CSSProperties = {
+        color: enabled == false ? "lightgrey" : "black"
+    }
 
     return (
         <div className={bem('container')}>
-            <div>
-                Hello <b>{locationToGreet}</b>
+            <Components.label {...props}/>
+            <div style={style}>
+                {greeting} <b>{locationToGreet}</b>
             </div>
         </div>
     );
